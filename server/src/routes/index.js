d4e7e095b0704a99cfd7b3c1e5b50f39db887f0d
Pedro Henrique <pedropires2603@gmail.com>
2022-04-03 15:42:53 -0300

    Update: added route get /usuario

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,5 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { getAll } from '../controllers/usuarioController';
 
 // Cria um novo objeto do tipo Router, que armazena todas as rotas criadas e que deverão
 // ser utilizadas pelo servidor.
@@ -6,8 +7,11 @@ const routes = new Router();
 
 // A primeira rota é a rota raiz (/)
 routes.get('/', (req, res) => {
-    res.status(200).json({ok: 'connected'});
+    res.status(200).json({ ok: 'connected' });
 });
+
+// Rota que retorna todos os usuários na base de dados
+routes.get('/usuario', getAll);
 
 // Exporta todas as rotas criadas dentro deste arquivo, na classe Routes() do express.
 export default routes;
